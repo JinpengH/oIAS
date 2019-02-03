62c351be95206d88ab114b2b56971c22272c8101
lofnheart <jinpengh@usc.edu>
2019-02-03 14:29:04 -0800

    Change of main page

--- untitled/routes/index.js
+++ untitled/routes/index.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/login1', function(req, res, next) {
-    res.render('index', { title: 'Login' });
-});
-
-router.get('/login2', function(req, res, next) {
-    res.render('login2', { title: 'Login' });
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Login' });
 });
 
 router.get('/welcome', function(req, res, next) {
