48678998ad283d291550256fbfa9e0c727b4ee5c
lofnheart <jinpengh@usc.edu>
2019-02-05 15:43:46 -0800

    Main page UI

--- untitled/public/javascripts/main.js
+++ untitled/public/javascripts/main.js
@@ -2,10 +2,9 @@ var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 
+    $('#sidebar_button').on('click', function() {
+        $('.content').toggleClass('isOpen');
+    });
 
-
-}
+};
