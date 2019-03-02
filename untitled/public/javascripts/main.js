b4723151fe15da8b93a6a5290785a290e46067ab
lofnheart <jinpengh@usc.edu>
2019-03-01 17:03:12 -0800

    adding new package html-pdf

--- untitled/public/javascripts/main.js
+++ untitled/public/javascripts/main.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 
     getSubmissions();
 
+
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
 
@@ -11,6 +14,14 @@ window.onclick = function(event) {
 
     $('#float_button').on('click', function() {
         $('.submission_form').fadeIn();
+    });
+
+    $(".preview.list_button").on('click',function(){
+        $(".modal").fadeIn();
+    });
+
+    $(".preview.list_button_cancel").on('click',function(){
+        $(".modal").fadeOut();
     });
 
     $('.forms_buttons-cancel').on('click', function() {
