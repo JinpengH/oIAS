991d723059be4e55a8cd2d4e80f60024da0ecea9
lofnheart <jinpengh@usc.edu>
2019-03-25 15:43:25 -0700

    adding overview client side script

---
+++ untitled/public/javascripts/overview.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+    $('#float_button').on('click', function() {
+        $('.submission_form').fadeIn();
+    });
+
+
+
+    $('.forms_buttons-cancel').on('click', function() {
+        $('.submission_form').fadeOut();
+    });
+
+});
+//changing status
+
+
